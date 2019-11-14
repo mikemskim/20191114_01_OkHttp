e5f6df94c31ab2d9df328c62ab6ff12384d2ad5f
mikemskim <mskim@steconsulting.co.kr>
2019-11-14 21:51:25 +0900

    회원가입 화면 그리기

---
+++ app/src/main/java/com/tj/a20191114_01_okhttp/SignUpActivity.kt
@@ -0,0 +1,22 @@
+package com.tj.a20191114_01_okhttp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        setupEvent()
+        setValue()
+    }
+
+    override fun setupEvent() {
+    }
+
+    override fun setValue() {
+    }
+}
